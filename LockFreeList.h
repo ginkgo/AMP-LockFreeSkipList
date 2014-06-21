c84f77b3a13f7d8ad7fba345a14f9c0ede755e23
adamp <adamp@laptophead.(none)>
2014-06-21 17:57:41 +0200

    initial LockFreeList commit

---
+++ LockFreeList.h
@@ -0,0 +1,157 @@
+#ifndef LOCKFREELIST_HPP
+#define LOCKFREELIST_HPP
+
+#include <atomic>
+#include "tagged_ptr.h"
+
+template <typename Pheet, typename T>
+class LockFreeList
+{
+    class Node;
+    typedef typename boost::lockfree::detail::tagged_ptr<Node> tagged_node_ptr;
+
+    class Node {
+        std::atomic<tagged_node_ptr> _next;
+
+    public:
+        T val;
+
+        Node(const T& val)
+            : val(val) { }
+
+        void nextStore(tagged_node_ptr& ptr) {
+            _next.store(ptr);
+        }
+
+        tagged_node_ptr next() {
+            tagged_node_ptr ptr = _next.load();
+            return ptr;
+        }
+
+        tagged_node_ptr next(bool& marked) {
+            tagged_node_ptr ptr = next();
+            marked = (ptr.get_tag() & 0x8000) == 0x8000;
+            return ptr;
+        }
+
+        bool markNext(tagged_node_ptr& ptr) {
+            auto tag = ptr.get_tag();
+            ++tag;
+            tag = tag | 0x8000;
+            tagged_node_ptr newptr(ptr.get_ptr(), tag);
+            return _next.compare_exchange_strong(ptr, newptr);
+        }
+
+        bool nextCAS(tagged_node_ptr& expected, tagged_node_ptr& ptr) {
+            auto tag = ptr.get_tag();
+            if(tag & 0x7FFF)
+                tag = tag & 0x8000;
+            ++tag;
+            ptr.set_tag(tag);
+            return _next.compare_exchange_strong(expected, ptr);
+        }
+    };
+
+    tagged_node_ptr head;
+
+    void find(tagged_node_ptr& pred, tagged_node_ptr& curr, T val) {
+        bool marked;
+        tagged_node_ptr succ;
+
+        retry:
+        while(true) {
+            pred = head;
+            curr = pred->next();
+            while(true) {
+                if(curr.get_ptr() == nullptr)
+                    return;
+
+                succ = curr->next(marked);
+                while(marked) {
+                    if(!pred->nextCAS(curr, succ))
+                        goto retry;
+                    curr = succ;
+                    succ = curr->next(marked);
+                }
+                if(curr->val >= val)
+                    return;
+                pred = curr;
+                curr = succ;
+            }
+        }
+    }
+
+public:
+    LockFreeList() {
+        head.set_ptr(new Node(T()));
+        tagged_node_ptr ptr(nullptr);
+        head->nextStore(ptr);
+    }
+
+    bool add(const T& val) {
+        tagged_node_ptr pred;
+        tagged_node_ptr curr;
+        std::unique_ptr<Node> ptr(new Node(val));
+        tagged_node_ptr node(ptr.get());
+
+        do {
+            find(pred, curr, val);
+            if(curr.get_ptr() != nullptr && curr->val == val)
+                return false;
+            node->nextStore(curr);
+        } while(!pred->nextCAS(curr, node));
+
+        ptr.release();
+        return true;
+    }
+
+    bool remove(const T& val) {
+        tagged_node_ptr pred;
+        tagged_node_ptr curr;
+        tagged_node_ptr succ;
+
+        while(true) {
+            find(pred, curr, val);
+            if(curr.get_ptr() == nullptr || curr->val != val)
+                return false;
+            succ = curr->next();
+            if(!curr->markNext(succ))
+                continue;
+            if(pred->nextCAS(curr, succ))
+                return true;
+        }
+    }
+
+    bool contains(const T& val) {
+        bool marked;
+        tagged_node_ptr curr;
+
+        curr = head->next(marked);
+        while(curr.get_ptr() != nullptr && curr->val < val) {
+            curr = curr->next(marked);
+        }
+        if(curr.get_ptr() == nullptr)
+            return false;
+        return (curr->val == val && !marked);
+    }
+
+    size_t size() {
+        bool marked;
+        size_t s = 0;
+        tagged_node_ptr curr;
+
+        curr = head->next(marked);
+        while(curr.get_ptr() != nullptr) {
+            if(!marked)
+                ++s;
+            curr = curr->next(marked);
+        }
+        return s;
+    }
+
+    static void print_name() {
+        std::cout << "LockFreeList";
+    }
+};
+
+#endif //LOCKFREELIST_HPP
